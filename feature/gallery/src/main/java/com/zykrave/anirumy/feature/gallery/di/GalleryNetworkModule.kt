3ec2ccb87db2498d0884bd5a0ecae668f052d48e
Zykrave <you@example.com>
2026-08-30 19:00:07 +0600

    Register WaifuImApi Retrofit singleton in GalleryNetworkModule

--- feature/gallery/src/main/java/com/zykrave/anirumy/feature/gallery/di/GalleryNetworkModule.kt
+++ feature/gallery/src/main/java/com/zykrave/anirumy/feature/gallery/di/GalleryNetworkModule.kt
@@ -1,6 +1,7 @@
 package com.zykrave.anirumy.feature.gallery.di
 
 import com.zykrave.anirumy.feature.gallery.data.remote.NekosBestApi
+import com.zykrave.anirumy.feature.gallery.data.remote.WaifuImApi
 import com.zykrave.anirumy.feature.gallery.data.repository.GalleryRepository
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -13,6 +14,7 @@ import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import retrofit2.create
 
 private const val NEKOS_BEST_BASE_URL = "https://nekos.best/api/v2/"
+private const val WAIFU_IM_BASE_URL = "https://api.waifu.im/"
 
 val galleryNetworkModule = module {
     single<NekosBestApi> {
@@ -20,6 +22,17 @@ val galleryNetworkModule = module {
         val json = Json { ignoreUnknownKeys = true }
         Retrofit.Builder()
             .baseUrl(NEKOS_BEST_BASE_URL)
+            .client(client)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create()
+    }
+
+    single<WaifuImApi> {
+        val client: OkHttpClient = get(named("rest"))
+        val json = Json { ignoreUnknownKeys = true }
+        Retrofit.Builder()
+            .baseUrl(WAIFU_IM_BASE_URL)
             .client(client)
             .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
